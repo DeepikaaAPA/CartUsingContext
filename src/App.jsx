48303436713f3a1261d468bb27c25ea8226a4cbf
DeepikaaAPA <deepikaarjun@gmail.com>
2024-06-29 20:35:17 +0530

    add: total as context

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useEffect, useState } from "react";
+import {useContext, createContext ,useEffect, useState } from "react";
 import products from "./Data/products.json";
 
 import "./App.css";
-
+const TotalContext = createContext();
 function App() {
   const [cart, setCart] = useState(products);
+  const [total, setTotal] = useState(1000);
   return (
-    <>
+    <TotalContext.Provider value={total}>
       <h1>Shopping Cart</h1>
       <div className="container">
         {cart.map((p) => (
@@ -14,27 +15,31 @@ function App() {
         ))}
       </div>
       <Total />
-    </>
+      </TotalContext.Provider>
   );
 }
 function Total() {
+  const total = useContext(TotalContext);
   return (
     <>
       <div className="total">
-        <h2>Total: Rs.abcfdefgh</h2>
+        <h2>Total: Rs.{total}</h2>
       </div>
     </>
   );
 }
 function ItemCard({ item }) {
   const [qty, setQty] = useState(1);
-  const [price,setPrice]=useState(item.price)
-  useEffect(()=>{setPrice(qty*item.price)},[qty])
+  const [price, setPrice] = useState(item.price);
+  
+  useEffect(() => {
+    setPrice(qty * item.price);
+  }, [qty]);
   return (
     <div className="item-card">
       <div className="left">
         <h3>{item.title} </h3>
-        <img src={item.thumbnail}></img>
+        {/* <img src={item.thumbnail}></img> */}
         <p>{item.description}</p>
         <h4>Price per item: Rs.{item.price}</h4>
       </div>
